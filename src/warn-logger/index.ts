90c8dc44ed83187b0666191163397e393aee5edf
Anwar Ahmed <nileking2007@gmail.com>
2025-07-04 17:41:39 +0300

    chore : add comment at the top to indicate path of index page

--- src/warn-logger/index.ts
+++ src/warn-logger/index.ts
@@ -1,16 +1,14 @@
+// warn-logger/index.ts
 import { createLogEntry } from "../log-entry";
-import { safeAppendToLog  } from "../file-manager";
-import { extractRequestData ,ApiRequestData} from "../request-utils";
-import { LogLevel, ApiError } from "../types";
-
-
+import { safeAppendToLog } from "../file-manager";
+import { LogLevel } from "../types";
 
 /**
  * Logs a warning message to the log file.
  */
 export async function logWarn(
   message: string,
-  details?: Record<string, unknown>
+  details?: Record<string, unknown>,
 ): Promise<void> {
   const logEntry = createLogEntry(LogLevel.WARN, message, details as any);
   await safeAppendToLog(JSON.stringify(logEntry));
